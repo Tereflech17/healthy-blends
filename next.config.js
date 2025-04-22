e1361730ae4d074c62e74595b05726437214eebb
tereflech17 <tereflech17@gmail.com>
2025-04-21 22:55:01 -0400

    typo fix

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.css$/,
-      use: ["style-loader", "css-loader", "postcss-loader"],,
+      use: ["style-loader", "css-loader", "postcss-loader"],
     });
     return config;
   }
